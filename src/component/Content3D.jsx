360ab6999d5ab92881afc8cd1894a19398362f84
janjao937 <janjao937@gmail.com>
2023-09-03 17:12:23 +0700

    init repo

---
+++ src/component/Content3D.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { useFBX } from "@react-three/drei";
+const Content3D=()=>{
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default Content3D;
